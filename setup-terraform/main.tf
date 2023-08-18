1ddd75dad5f4745fdbc14780aa0a245092c0b239
= <xysiek99@gmail.com>
2023-08-18 20:21:24 +0200

    Adding user with ssh key auth

--- setup-terraform/main.tf
+++ setup-terraform/main.tf
@@ -3,14 +3,36 @@ resource "linode_instance" "php_vm" {
   type            = "g6-nanode-1"
   image           = "linode/debian11"
   region          = "eu-central"
-  authorized_keys = [var.ssh_public_key]
+  authorized_keys = [trimspace(file(var.ssh_public_key_path))]
+
+provisioner "remote-exec" {
+    inline = [
+      "useradd -m -s /bin/bash -G sudo ${var.technician_username}",
+      
+      "mkdir /home/${var.technician_username}/.ssh",
+      "echo '${trimspace(file(var.technician_public_key_path))}' > /home/${var.technician_username}/.ssh/authorized_keys",
+      "chown -R ${var.technician_username}:${var.technician_username} /home/${var.technician_username}/.ssh",
+      "chmod 700 /home/${var.technician_username}/.ssh",
+      "chmod 600 /home/${var.technician_username}/.ssh/authorized_keys",
+
+      "echo '${var.technician_username} ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers.d/${var.technician_username}",     
+      "systemctl restart ssh"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = file(var.ssh_private_key_path)
+      host        = self.ip_address
+    }
+}
 
   provisioner "local-exec" {
     command = templatefile("config-ssh-wikijs.tpl", {
       custom_hostname = var.instance_name
       hostname        = self.ip_address,
-      user            = "root",
-      identityfile    = var.wikijs_public_key_path
+      user            = var.technician_username ,
+      identityfile    = var.technician_private_key_path
     })
     interpreter = ["bash", "-c"]
   }
--- setup-terraform/variables.tf
+++ setup-terraform/variables.tf
@@ -5,22 +5,38 @@ variable "linode_token" {
 }
 
 variable "instance_name" {
-  type = string
+  type    = string
   default = "php_app_vm"
 }
 
 variable "root_pass" {
-  type        = string
-  default     = "t4_test"
+  type    = string
+  default = "t4_test"
 }
 
-variable "ssh_public_key" {
-  type        = string
-  sensitive   = true
-  description = "Key for ssh authentication"
-}
-
-variable "wikijs_public_key_path" {
+variable "ssh_public_key_path" {
   type    = string
   default = "~/.ssh/wikijs_user_key.pub"
+}
+
+variable "ssh_private_key_path" {
+  type    = string
+  default = "~/.ssh/wikijs_user_key"
+}
+
+# Add technician user and his key
+
+variable "technician_username" {
+  type    = string
+  default = "inzynier"
+}
+
+variable "technician_private_key_path" {
+  type    = string
+  default = "~/.ssh/inzynier_key"
+}
+
+variable "technician_public_key_path" {
+  type    = string
+  default = "~/.ssh/inzynier_key.pub"
 }
