4d61f80c167474e6b4f0cdb35b01b86633107b88
= <xysiek99@gmail.com>
2023-09-25 14:42:14 +0200

    TF module for multiple VMs configuration

--- setup-terraform/main.tf
+++ setup-terraform/main.tf
@@ -1,50 +1,25 @@
-resource "linode_instance" "php_vm" {
-  label           = var.instance_name
-  type            = "g6-nanode-1"
-  image           = "linode/debian11"
-  region          = "eu-central"
-  root_pass       = var.root_password
-
-  provisioner "remote-exec" {
-    inline = [
-      "useradd -m -s /bin/bash -G sudo ${var.technician_username}",
-
-      "mkdir /home/${var.technician_username}/.ssh",
-      "echo '${trimspace(file(var.technician_public_key_path))}' > /home/${var.technician_username}/.ssh/authorized_keys",
-      "chown -R ${var.technician_username}:${var.technician_username} /home/${var.technician_username}/.ssh",
-      "chmod 700 /home/${var.technician_username}/.ssh",
-      "chmod 600 /home/${var.technician_username}/.ssh/authorized_keys",
-
-      "echo '${var.technician_username} ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers.d/${var.technician_username}",
-      "systemctl restart ssh"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "root"
-      password    = var.root_password
-      host        = self.ip_address
-    }
+module "php_vm" {
+  source                      = "./my_module"
+  linode_config = {
+    instance_name              = "docker_registry_vm"
+    root_password              = var.root_password
+    technician_username        = var.technician_username
+    technician_public_key_path = var.technician_public_key_path
+    technician_private_key_path = var.technician_private_key_path
+    ansible_host_file          = var.ansible_host_file
+    linode_token               = var.linode_token
   }
+}
 
-  provisioner "local-exec" {
-    command = templatefile("config-ssh.tpl", {
-      custom_hostname = var.instance_name,
-      hostname        = self.ip_address,
-      user            = var.technician_username,
-      identityfile    = var.technician_private_key_path
-    })
-    interpreter = ["bash", "-c"]
-  }
-
-  provisioner "local-exec" {
-    command = templatefile("create-ansible-hosts.tpl", {
-      hostsfile    = var.ansible_host_file,
-      hostgroup    = var.instance_name,
-      hostname     = self.ip_address,
-      user         = var.technician_username,
-      identityfile = var.technician_private_key_path
-    })
-    interpreter = ["bash", "-c"]
+module "docker_registry_vm" {
+  source                      = "./my_module"
+  linode_config = {
+    instance_name              = "php_vm"
+    root_password              = var.root_password
+    technician_username        = var.technician_username
+    technician_public_key_path = var.technician_public_key_path
+    technician_private_key_path = var.technician_private_key_path
+    ansible_host_file          = var.ansible_host_file
+    linode_token               = var.linode_token
   }
 }
---
+++ setup-terraform/my_module/variables.tf
@@ -0,0 +1,4 @@
+variable "linode_config" {
+  description = "Configuration map for the Linode instance."
+  type        = map(string)
+}
